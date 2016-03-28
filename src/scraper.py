77610f0935342d68ebe13ec7a18f2b42adb2e0ae
icarrera <irismcarrera@gmail.com>
2016-03-27 17:22:43 -0700

    Moved King County health data into src folder. Added load_inspection function.

--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import requests
+import io
+from bs4 import BeautifulSoup
 
+
+DATA = 'kc_health_data.html'
 INSPECTION_DOMAIN = "http://info.kingcounty.gov"
 INSPECTION_PATH = "/health/ehs/foodsafety/inspections/Results.aspx/"
 # see uri query output for inspection params
@@ -25,18 +29,21 @@ INSPECTION_PARAMS = {
 }
 
 
-def get_inspection_page(data):
+def get_inspection_page(**kwargs):
     """Fetch a set of search results."""
     url = INSPECTION_DOMAIN + INSPECTION_PATH
     params = INSPECTION_PARAMS.copy()
-    for key, val in data.items():
+    for key, val in kwargs.items():
         if key in INSPECTION_PARAMS:
             params[key] = val
     resp = requests.get(url, params=params, timeout=10)
     resp.raise_for_status()
     return resp.content, resp.encoding
 
-def load_inspection_page(data):
-    """Read file from disk and return the content."""
-    
-# get_inspection_page(INSPECTION_PARAMS)
+
+def load_inspection_page(from_file):
+    """Load inspection page from  file."""
+    from_file = io.open(from_file, encoding='utf-8', mode='read')
+    text = from_file.read()
+    from_file.close()
+    return text, 'utf-8'
