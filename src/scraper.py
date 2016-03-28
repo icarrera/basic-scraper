02943963e171265df3527790a37f1d2f5003d8a4
icarrera <irismcarrera@gmail.com>
2016-03-27 19:19:21 -0700

    Removed the from_encoding=encoding parameter from the parsed_source function. This addressed the Type Error I was getting when running scraper.py.

--- src/scraper.py
+++ src/scraper.py
@@ -1,19 +1,20 @@
-# -*- coding: utf-8 -*-
+"""Basic html scraper for the King County food safety inspection website."""
+
 import requests
 import io
 from bs4 import BeautifulSoup
+import sys
+import re
 
 
-DATA = 'kc_health_data.html'
 INSPECTION_DOMAIN = "http://info.kingcounty.gov"
-INSPECTION_PATH = "/health/ehs/foodsafety/inspections/Results.aspx/"
-# see uri query output for inspection params
+INSPECTION_PATH = "/health/ehs/foodsafety/inspections/Results.aspx"
 INSPECTION_PARAMS = {
     'Output': 'W',
     'Businesss_Name': '',
     'Business_Address': '',
-    'Latitude': '',
     'Longitude': '',
+    'Latitude': '',
     'City': '',
     'Zip_Code': '',
     'Inspection_Type': 'All',
@@ -24,8 +25,7 @@ INSPECTION_PARAMS = {
     'Violation_Red_Points': '',
     'Violation_Descr': '',
     'Fuzzy_Search': 'N',
-    'Sort': 'B'
-
+    'Sort': 'H'
 }
 
 
@@ -43,7 +43,7 @@ def get_inspection_page(**kwargs):
 
 def load_inspection_page(from_file):
     """Load inspection page from  file."""
-    from_file = io.open(from_file, encoding='utf-8', mode='read')
+    from_file = io.open(from_file, encoding='utf-8', mode='r')
     text = from_file.read()
     from_file.close()
     return text, 'utf-8'
@@ -51,5 +51,25 @@ def load_inspection_page(from_file):
 
 def parse_source(html, encoding='utf-8'):
     """Setup the HTML as DOM nodes for scraping."""
-    parsed = BeautifulSoup(html, 'html5lib', from_encoding=encoding)
+    parsed = BeautifulSoup(html, 'html5lib')
     return parsed
+
+
+def extract_data_listings(html):
+    """Take parsed HTML and return list of restaurant listing container nodes."""
+    id_finder = re.compile(r'PR[\d]+~')
+    return html.find_all('div', id=id_finder)
+
+
+if __name__ == '__main__':
+    kwargs = {
+        'Inspection_Start': '2/1/2013',
+        'Inspection_End': '2/1/2015',
+        'Zip_Code': '98144'
+    }
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        html, encoding = load_inspection_page('inspection_page.html')
+    else:
+        html, encoding = get_inspection_page(**kwargs)
+    doc = parse_source(html, encoding)
+    print(doc.prettify(encoding=encoding))
