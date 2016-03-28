b79aca0926a0d5bbc92ab16839088f689b0cdc18
icarrera <irismcarrera@gmail.com>
2016-03-27 23:40:51 -0700

    started working on step2

--- src/scraper.py
+++ src/scraper.py
@@ -128,13 +128,13 @@ def extract_score_data(elem):
         return data
 
 
-if __name__ == '__main__':
+def generate_results(test=False):
     kwargs = {
         'Inspection_Start': '2/1/2013',
         'Inspection_End': '2/1/2015',
         'Zip_Code': '98107'
     }
-    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+    if test:
         html, encoding = load_inspection_page('inspection_page.html')
     else:
         html, encoding = get_inspection_page(**kwargs)
@@ -143,4 +143,11 @@ if __name__ == '__main__':
     for listing in listings:
         metadata = extract_restaurant_metadata(listing)
         score_data = extract_score_data(listing)
-        print(score_data)
+        metadata.update(score_data)
+        yield metadata
+
+
+if __name__ == '__main__':
+    test = len(sys.argv) > 1 and sys.argv[1] == 'test'
+    for result in generate_results(test):
+        print(result)
